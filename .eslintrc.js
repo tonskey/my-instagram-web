670c9755d19be8ee6fa9634883a3144355372d34
Anton Kharhonov <vachovski4@gmail.com>
2021-12-13 00:11:05 +0000

    install eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:react/jsx-runtime'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'import/prefer-default-export': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'import/no-extraneous-dependencies': 'off',
+  },
+};
